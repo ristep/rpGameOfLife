4fd052614729ede4c59294d01573bd3ee88078bb
ristep <ristepan@gmail.com>
2019-12-02 01:58:16 +0100

    commit

--- src/components/about.test.jsx
+++ src/components/about.test.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+
 import store from '../redux/store';
 import AboutDialog from './about';
+import { render } from  "@testing-library/react";
+
+describe('With React Testing Library', () => {
+	var initialState = store.getState();
+	const mockStore = configureStore();
+	initialState = {...initialState, uiState: { ...initialState.uiState, aboutDialogState: true } };
+	// console.log( initialState );
+	let testStore = mockStore(initialState);
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
-	ReactDOM.render(
+	const result = render(
 		<Provider store={store}>
 			<AboutDialog />
 		</Provider>, div);
+	// console.log(result.type);	
+	expect(result.type).toBe(undefined);
 	ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+	const div = document.createElement('div');
+	const result = render(
+		<Provider store={testStore}>
+			<AboutDialog />
+		</Provider>, div);
+	console.log(result.type);	
+	expect(result.type).not.toBeNull()
+	ReactDOM.unmountComponentAtNode(div);
+});
+
 });
--- src/elemens/cell.test.jsx
+++ src/elemens/cell.test.jsx
@@ -1,35 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { useDispatch, Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import store from '../redux/store';
 import Cell from './cell';
-import { toggleCell } from 'redux/actions';
+import configureStore from 'redux-mock-store'
+//import { render } from 'react-testing-library'
 
 describe('With React Testing Library', () => {
+	var initialState = store.getState();
+	const mockStore = configureStore();
+	const cells = [[ 0,0,0,1 ],[ 1,0,0,0 ],[ 0,1,0,0 ],[ 0,0,1,0 ]];  
+	initialState = {...initialState, world:{ ...initialState.world, conf: { rows: 4, colls: 4, cellSize: 12, cellTipe: 'image' }, cells:cells} };
+	// console.log( initialState );
+	let testStore,wrapper;
+	testStore = mockStore(initialState);
 	
-it('renders without crashing for 10,10', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(
-		<Provider store={store}>
-			<Cell cord={{ row:10, col:10 }} />
-		</Provider>, div);
-	ReactDOM.unmountComponentAtNode(div);
-});
-
 it('renders without crashing for 0,0', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(
-		<Provider store={store}>
+		<Provider store={testStore}>
 			<Cell cord={{ row:0, col:0 }} />
 		</Provider>, div);
 	ReactDOM.unmountComponentAtNode(div);
 });
 
+it('renders without crashing for 2,0', () => {
+	const div = document.createElement('div');
+	ReactDOM.render(
+		<Provider store={testStore}>
+			<Cell cord={{ row:2, col:0 }} />
+		</Provider>, div);
+	ReactDOM.unmountComponentAtNode(div);
+});
+ 
 it('renders without crashing for 5,5 as an Apple', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(
-		<Provider store={{...store, conf:{ cellTipe:'image'}}} >
-			<Cell cord={{ row:5, col:5 }} />
+		<Provider store={testStore} >
+			<Cell cord={{ row:1, col:3 }} />
 		</Provider>, div);
 	ReactDOM.unmountComponentAtNode(div);
 });
